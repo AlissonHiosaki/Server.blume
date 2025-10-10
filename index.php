c680a92283668220f7c93c45d91b89b9c4c59807
Alisson Hiosaki <92666141+AlissonHiosaki@users.noreply.github.com>
2025-10-10 19:02:28 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-
+<html lang="pt-br">
+<html>
+  <head>
+    <title>apiweb</title>
+  </head>
+  <body>
+    <h1>
+      <input class="na-app">
+      <label>A B C </label>
+    </h1>
+  </body>
+</html>
